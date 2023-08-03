cc8f19974d796035e571e98d19ce3324bbb18427
angelagongli <angelagongli@gmail.com>
2023-08-03 00:16:48 -0500

    add models

---
+++ fileloader/models/File.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fileloader.models
+{
+    public class File
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public int FileLength { get; set; }
+        public string Source { get; set; }
+        public string Destination { get; set; }
+        public int LoadType { get; set; }
+        public DateTime LoadTime { get; set; }
+        public int LoadedStatus { get; set; }
+    }
+}
---
+++ fileloader/models/Row.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fileloader.models
+{
+    public class Row
+    {
+        public int FileId { get; set; }
+        public int RowNumber { get; set; }
+        public string SenderRow { get; set; }
+    }
+}
